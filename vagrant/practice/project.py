9f7a9640fd4b7dbc2c1111b36bbb69767c415c6f
katebron <kate.bronstad@tufts.edu>
2016-04-13 19:40:20 -0400

    include flash messages

--- vagrant/practice/project.py
+++ vagrant/practice/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template, request, redirect
+from flask import Flask, url_for, render_template, request, redirect, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -29,6 +29,7 @@ def newMenuItem(restaurant_id):
 		newItem = MenuItem(name = request.form['name'], restaurant_id =  restaurant_id)
 		session.add(newItem)
 		session.commit()
+		flash("New menu item added!")
 		return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
 	else:
 		return render_template('newmenuitem.html', restaurant_id = restaurant_id)
@@ -37,10 +38,20 @@ def newMenuItem(restaurant_id):
 # note in actual solutions the edit part is on the other side of the url from the id
 # like '/restaurants/int/int/edit'
 # same with delete
-@app.route('/restaurants/edit_item/<int:restaurant_id>/<int:menu_id>/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit',
+           methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    return "page to edit a menu item. Task 2 complete!"
-
+    editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        session.commit()
+        flash("Menu item edited!")
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template(
+            'editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=editedItem)
 # Task 3: Create a route for deleteMenuItem function here
 
 @app.route('/restaurants/delete_item/<int:restaurant_id>/<int:menu_id>/', methods=['GET','POST'])
@@ -49,15 +60,18 @@ def deleteMenuItem(restaurant_id, menu_id):
 	if request.method == 'POST':
 		session.delete(itemToDelete)
 		session.commit()
+		flash("Menu item deleted!")
 		return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
 	else:
-		#in actual soln, they didn't have to 
+		#couldn't get the instructor soln to work
 		return render_template('deletemenuitem.html', item = itemToDelete, restaurant_id = restaurant_id)	
     #return "page to delete a menu item. Task 3 complete!"
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+	#make a secret key so can have a session
+	app.secret_key = 'super_secret_key'
+	app.debug = True
+	app.run(host='0.0.0.0', port=5000)
 
 
 
