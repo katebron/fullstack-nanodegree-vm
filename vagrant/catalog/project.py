451db246213b48067c3e179289adbcd753679490
katebron <kate.bronstad@tufts.edu>
2016-06-20 16:42:25 -0400

    up to initial gconnect settings

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
-from sqlalchemy import create_engine, asc
+from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker
 from music_db_setup import Base, Playlist, Song, User
 from flask import session as login_session
@@ -14,9 +14,9 @@ import requests
 
 app = Flask(__name__)
 
-#CLIENT_ID = json.loads(
- #   open('client_secrets.json', 'r').read())['web']['client_id']
-APPLICATION_NAME = "Music Playlists Application"
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Custom Playlists"
 
 
 # Connect to Database and create database session
@@ -26,12 +26,164 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-# Show all restaurants
+# User Helper Functions
+
+
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
+
+
+# Create anti-forgery state token
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    # return "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
+
+
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_credentials = login_session.get('credentials')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+    # ADD PROVIDER TO LOGIN SESSION
+    login_session['provider'] = 'google'
+
+    # see if user exists, if it doesn't make a new one
+    user_id = getUserID(data["email"])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
+
+# JSON API to view Restaurant Information
+@app.route('/playlist/JSON')
+def playlistsJSON():
+    playlists = session.query(Playlist).all()
+    return jsonify(playlists=[p.serialize for p in playlists])
+
+# JSON API to view songs in a playlist
+@app.route('/playlist/<int:playlist_id>/songs_in_playlist/JSON')
+def playlistSongsJSON(playlist_id):
+    playlist = session.query(Playlist).filter_by(id=playlist_id).one()
+    items = session.query(Song).filter_by(
+        playlist_id=playlist_id).all()
+    return jsonify(Songs=[i.serialize for i in items])
+
+# JSON API to view all songs
+@app.route('/playlist/all_songs/JSON')
+def allSongsJSON():
+    songs = session.query(Song).all()
+    return jsonify(songs=[s.serialize for s in songs])
+
+
+# Show all playlists
 @app.route('/')
 @app.route('/playlist/')
 def showPlaylists():
     playlists = session.query(Playlist).order_by(asc(Playlist.title))
-    return render_template('playlist.html', playlists=playlists)
+    songs = session.query(Song).order_by(desc(Song.id)).limit(2)
+    if 'username' not in login_session:
+        return render_template('public_playlists.html', playlists=playlists)
+    else:
+        return render_template('playlist.html', playlists=playlists, items = songs)
 
 @app.route('/playlist/new/', methods=['GET', 'POST'])
 def newPlaylist():
@@ -50,14 +202,13 @@ def newPlaylist():
 @app.route('/playlist/<int:playlist_id>/songs/')
 def showSongs(playlist_id):
     playlist = session.query(Playlist).filter_by(id=playlist_id).one()
-    #creator = getUserInfo(restaurant.user_id)
+    creator = getUserInfo(playlist.user_id)
     items = session.query(Song).filter_by(
         playlist_id=playlist_id).all()
-    '''if 'username' not in login_session or creator.id != login_session['user_id']:
-        return render_template('publicmenu.html', items=items, restaurant=restaurant, creator=creator)
+    if 'username' not in login_session or creator.id != login_session['user_id']:
+        return render_template('public_list_of_songs.html', items=items, playlist=playlist, creator=creator)
     else:
-        '''
-    return render_template('list_of_songs.html', items=items, playlist=playlist)
+        return render_template('list_of_songs.html', items=items, playlist=playlist)
 
 @app.route('/playlist/<int:playlist_id>/edit/', methods=['GET', 'POST'])
 def editPlaylist(playlist_id):
@@ -69,11 +220,13 @@ def editPlaylist(playlist_id):
     #    return "<script>function myFunction() {alert('You are not authorized to edit this restaurant. Please create your own restaurant in order to edit.');}</script><body onload='myFunction()''>"
     if request.method == 'POST':
         if request.form['title']:
-            editedPlaylist.name = request.form['title']
+            editedPlaylist.title = request.form['title']
+            editedPlaylist.description = request.form['description']
+            session.commit()
             flash('Playlist Successfully Edited %s' % editedPlaylist.title)
             return redirect(url_for('showPlaylists'))
     else:
-        return render_template('editPlaylist.html', playlist=editedPlaylist)
+        return render_template('edit_playlist.html', playlist=editedPlaylist)
 
 # Create a new menu item
 @app.route('/playlist/<int:playlist_id>/song/new/', methods=['GET', 'POST'])
@@ -98,6 +251,7 @@ def newSong(playlist_id):
 def deletePlaylist(playlist_id):
     playlistToDelete = session.query(
         Playlist).filter_by(id=playlist_id).one()
+    songsToDelete = session.query(Song).filter_by(playlist_id = playlist_id).all()
     #if 'username' not in login_session:
      #   return redirect('/login')
     #if restaurantToDelete.user_id != login_session['user_id']:
